bafd32e6dc16189ee5cf1bbea8519f5760559c9e
T U <tylerletendre98@gmail.com>
2022-02-08 21:56:48 -0500

    trying to fix the header on the messageboard side of the page

--- legendary-applications-assessment/src/components/Header/header.jsx
+++ legendary-applications-assessment/src/components/Header/header.jsx
@@ -3,6 +3,12 @@ import "./header.css";
 function Header() {
   return (
     <div className="header-container">
+      <div className="company-image">
+        <img
+          src="http://www.pngmart.com/files/3/Stock-Market-Graph-Up-Transparent-PNG.png"
+          alt=""
+        />
+      </div>
       <div className="company-logo">
         <h3>Legendary Collections</h3>
         <p>The Organized Collectors App!</p>
